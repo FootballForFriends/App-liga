005e55b9517843218315c798180508400574632e
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-04-05 15:21:47 -0300

    Adicionando lista personalizada de elementos

--- src/componentes/table/index.tsx
+++ src/componentes/table/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Table, Popconfirm, Form, Input } from 'antd'
+import Avatar from 'antd/lib/avatar/avatar';
+import { UserOutlined } from '@ant-design/icons';
 
 const EditableContext = React.createContext<any>({});
 
@@ -72,24 +74,26 @@ const EditableCell = ({ title, editable, children, dataIndex, record, handleSave
 };
 
 interface IProps {
-
+  data: any[];
 }
+/*  [
+  {
+    key: "0",
+    name: "Jose champs",
+    age: "Pereba futebol clube"
+  },
+  {
+    key: "1",
+    name: "José não champs",
+    age: "Os perna quebrada futebool clube"
+  }
+]
+*/
 
-const CustomTable: React.FC<IProps> = () => {
+const CustomTable: React.FC<IProps> = ({ data }) => {
   const [dataSource, setDataSource] = React.useState({
-    data: [
-      {
-        key: "0",
-        name: "Jose champs",
-        age: "Pereba futebol clube"
-      },
-      {
-        key: "0",
-        name: "José não champs",
-        age: "Os perna quebrada futebool clube"
-      }
-    ],
-    count: 22
+    data: data,
+    count: data.length
   });
 
   const handleDelete = (key: any) => {
@@ -102,18 +106,25 @@ const CustomTable: React.FC<IProps> = () => {
 
   const columns = [
     {
+      title: "",
+      dataIndex: "icone",
+      width: "10%",
+      editable: false,
+      render: () => <Avatar icon={<UserOutlined />} />
+    },
+    {
       title: "Jogador",
-      dataIndex: "name",
+      dataIndex: "nome",
       width: "40%",
       editable: true
     },
     {
       title: "Time",
-      dataIndex: "age",
+      dataIndex: "time",
       width: "40%"
     },
     {
-      title: "operation",
+      title: "Ações",
       dataIndex: "operation",
       render: (text: any, record: any) =>
         dataSource.data.length >= 1 ? (
