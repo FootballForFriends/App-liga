701750b86d02b9f36b85a426dcbdbecb0e5fe140
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-04-05 15:11:33 -0300

    Adicionando opção de ser form

--- src/componentes/modal/index.tsx
+++ src/componentes/modal/index.tsx
@@ -1,23 +1,30 @@
-import { Button, Modal } from 'antd';
+import { Button, FormInstance, Modal } from 'antd';
 import React from 'react'
 
 interface IModalProps {
   buttonTitle: string;
   modalTitle: string;
+  form?: FormInstance<any>;
 }
 
-const ModalButton: React.FC<IModalProps> = ({ buttonTitle, modalTitle, children }) => {
+const ModalButton: React.FC<IModalProps> = ({ buttonTitle, modalTitle, children, form }) => {
+
   const [isModalVisible, setIsModalVisible] = React.useState(false);
 
   const showModal = () => setIsModalVisible(true);
-  const handleOk = () => setIsModalVisible(false);
+  const handleOk = () => {
+    if (form) {
+      form.submit()
+    }
+    setIsModalVisible(false);
+  };
   const handleCancel = () => setIsModalVisible(false);
 
   return (<>
     <Button type="primary" onClick={showModal}>
       {buttonTitle}
     </Button>
-    <Modal title={modalTitle} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+    <Modal title={modalTitle} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} okText='Confirmar' cancelText='Cancelar'>
       {children}
     </Modal>
   </>);
